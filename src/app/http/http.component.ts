455fad16b29873ec5c121712a577fb462abe32bd
matt charlton <mattcharlton33@gmail.com>
2020-10-28 16:42:45 -0700

    http example

--- src/app/http/http.component.ts
+++ src/app/http/http.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+//http
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-http',
@@ -6,8 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./http.component.scss']
 })
 export class HttpComponent implements OnInit {
+  jsonData;
 
-  constructor() { }
+  //remote json datatable array
+  jsonDataArray;
+
+  //remote json datatable object
+  jsonDataObject;
+
+  constructor(private http: HttpClient) {
+    this.http.get<any>('https://api.npms.io/v2/search?q=scope:angular').subscribe(arrayData => {
+      this.jsonDataArray = arrayData.results;
+      console.log('data', this.jsonDataArray);
+    })
+  }
 
   ngOnInit(): void {
   }
